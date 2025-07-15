ef8bc6d1ca202c5fe203714ffc2f4df763d05e2f
Adwil Castillo <adwil800@gmail.com>
2025-07-15 14:38:14 -0400

    Fixed package naming

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: 'natestrauser:publish-performant-counts',
-  version: '0.1.2',
+  name: 'adwil800:publish-performant-counts-async',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Publish counts of large collections efficently',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/nate-strauser/meteor-publish-performant-counts',
+  git: 'https://github.com/adwil800/meteor-publish-performant-counts',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.1');
+  api.versionsFrom('METEOR@3.0');
   api.addFiles('lib/server.js', 'server');
   api.addFiles('lib/client.js', 'client');
   api.export(['Counter']);
